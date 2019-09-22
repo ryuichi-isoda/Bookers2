990d019369f06b0b0e50345a889f163b8055b2e2
isoda <ryuichi.isoda1007@gmail.com>
2019-09-23 02:56:10 +0900

    [Add] explanation

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -40,6 +40,8 @@ Devise.setup do |config|
   # session. If you need permissions, you should implement that in a before filter.
   # You can also supply a hash where the value is a boolean determining whether
   # or not authentication should be aborted when the value is not present.
+  
+  # ここをnameに変更する！！
   config.authentication_keys = [:name]
 
   # Configure parameters from the request object used for authentication. Each entry
